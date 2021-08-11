f7fe1b13fd4f5745ddec664c44a9c5a53b6e5528
TamimIlyas <75974165+TamimIlyas@users.noreply.github.com>
2021-08-11 12:59:45 +0200

    Projekt version

--- src/test/java/com/h2/SavingsCalculator.java
+++ src/test/java/com/h2/SavingsCalculator.java
@@ -12,23 +12,33 @@ public class SavingsCalculator {
     }
 
     private float sumOfCredits() {
-        float sum = 0.0f;    //6
+        float sum = 0.0f;
         for (int i = 0; i <= credits.length; i++) {
-            sum = +credits[i];
-
-
+            sum = + credits[i];
         }
     }
     private float sumOfDebits() {
         float sum = 0.0f;
         for (int i = 0; i <= debits.length; i++) {
-            sum = +debits[i];
+            sum = + debits[i];
         }
     }
-    private static  int  remainingDaysInMonth(int data, LocalDate date){            //9, 10
-        LocalDate yearMonth = LocalDate.of(1, 2, 2);
+    private static  int  remainingDaysInMonth(LocalDate date) {            //9, 10
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
         int totalDaysInMonth = yearMonth.lengthOfMonth();
-        int remainingDays = date.getDayOfMonth();
-        return 0;
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
+        return remainingDays;
     }
+    public float calculate(){
+        return sumOfCredits() - sumOfDebits();
+
+    }
+    public static void main(String[] args){
+        String[] creditsAsString = args[0].split(",");
+        String[] debitsAsString = args[1].split(",");
+        float[] credits = new float .length
+
+
+    }
+
 }
