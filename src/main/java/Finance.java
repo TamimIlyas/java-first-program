c05fb7915e04d5a8f8e249e9aa77b0f0eb515ea6
TamimIlyas <75974165+TamimIlyas@users.noreply.github.com>
2021-08-12 17:16:46 +0200

    Projekt version

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Map;
 import com.h2.BestLoanRates;
 import com.h2.MortgageCalculator;
@@ -39,13 +40,23 @@ public class Finance {
             case MORTGAGE_CALCULATOR:
                 System.out.println("Finding your monthly payment ...");
                 MortgageCalculator.main(argumnets);
+                return;
 
 
         }
     }
     public static void main(String[] args){
         String command = args[0];
-
+        if (commandsToUsage.containsKey(command)){
+            System.out.println(command + ": command not found");
+            return;
+        }
+        boolean isValidCommand = validateCommandArguments(args);
+        if (isValidCommand != true){
+            commandsToUsage.get(args[0]);
+            return;
+        }
+        executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
     }
 
 }
